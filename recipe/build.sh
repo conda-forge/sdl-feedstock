a2898ea7487238cb7b70d0d73dc339a45de9fde2
Wolf Vollprecht <w.vollprecht@gmail.com>
2020-09-29 12:21:56 +0200

    try with unknown-linux

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,8 @@
+if [ "${target_platform}" == "linux-aarch64" ]; then
+    export HOST="aarch64-unknown-linux-gnu"
+    export BUILD="aarch64-unknown-linux-gnu"
+fi
+
 ./configure --prefix=$PREFIX
 make -j${CPU_COUNT}
 make install
