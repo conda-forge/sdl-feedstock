980429a4981f5a3d3243c0f6392f8b4f974eaa9b
Wolf Vollprecht <w.vollprecht@gmail.com>
2020-09-29 15:55:44 +0200

    fix aarch64

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,7 @@
 if [ "${target_platform}" == "linux-aarch64" ]; then
-    export HOST="aarch64-unknown-linux-gnu"
-    export BUILD="aarch64-unknown-linux-gnu"
+    EXTRA_ARGS="--host=aarch64-unknown-linux-gnu --build=aarch64-unknown-linux-gnu"
 fi
 
-./configure --prefix=$PREFIX
+./configure --prefix=$PREFIX ${EXTRA_ARGS}
 make -j${CPU_COUNT}
 make install
