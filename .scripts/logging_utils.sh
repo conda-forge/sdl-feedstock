a8451fafed135390bf0a87928aa14bdb233fa9a1
Tobias Fischer <info@tobiasfischer.info>
2021-04-08 12:27:08 +1000

    osx-arm fixed (#21)
    * migration: OSXArm
    
    * MNT: Re-rendered with conda-build 3.21.4, conda-smithy 3.9.0, and conda-forge-pinning 2021.04.03.05.20.29
    
    * Add gnuconfig
    
    * Update build.sh
    
    * Update build.sh
    
    * Update build.sh
    
    Co-authored-by: regro-cf-autotick-bot <36490558+regro-cf-autotick-bot@users.noreply.github.com>

--- .scripts/logging_utils.sh
+++ .scripts/logging_utils.sh
@@ -12,24 +12,19 @@ function startgroup {
             echo "##[group]$1";;
         travis )
             echo "$1"
-            echo -en 'travis_fold:start:'"${1// /}"'\r';;
-        github_actions )
-            echo "::group::$1";;
+            echo -en 'travis_fold:start:'"${1// /}"'\\r';;
         * )
             echo "$1";;
     esac
-} 2> /dev/null
+}
 
 function endgroup {
     # End a foldable group of log lines
     # Pass a single argument, quoted
-
     case ${CI:-} in
         azure )
             echo "##[endgroup]";;
         travis )
-            echo -en 'travis_fold:end:'"${1// /}"'\r';;
-        github_actions )
-            echo "::endgroup::";;
+            echo -en 'travis_fold:end:'"${1// /}"'\\r';;
     esac
-} 2> /dev/null
+}
