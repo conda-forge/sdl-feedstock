ac857b4644e1cd947c58fa360beafe4d683bc020
Silvio Traversaro <silvio@traversaro.it>
2022-03-11 12:59:14 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,13 +2,13 @@
 
 mkdir build && cd build
 
+# DYLIB_COMPAT_VERSION is set for compatibility with autotools builds
+# See https://github.com/conda-forge/sdl-feedstock/pull/6#issuecomment-1065039606
 cmake ${CMAKE_ARGS} -GNinja -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_PREFIX_PATH=$PREFIX \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_INSTALL_LIBDIR=lib \
       -DBUILD_SHARED_LIBS=ON \
-      # See https://github.com/conda-forge/sdl-feedstock/pull/6#issuecomment-1065039606
-      # For compatibility with autotools builds
       -DDYLIB_COMPAT_VERSION:STRING="12.0.0" \
       $SRC_DIR
 
