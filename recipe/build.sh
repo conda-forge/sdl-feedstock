6f5c1378b8f4f423d3a31f3eca8d233cdb3396b7
Yann de Mont-Marin <ydemontm@mac.home>
2025-02-13 14:05:03 +0100

    Update sh recipe to sdl3

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,20 +1,14 @@
 #!/bin/sh
 
-mkdir build && cd build
-
-# DYLIB_COMPAT_VERSION is set for compatibility with autotools builds
-# See https://github.com/conda-forge/sdl-feedstock/pull/6#issuecomment-1065039606
-cmake ${CMAKE_ARGS} -GNinja -DCMAKE_INSTALL_PREFIX=$PREFIX \
+# Configure using the CMakeFiles
+cmake -S . -B build ${CMAKE_ARGS} \
       -DCMAKE_PREFIX_PATH=$PREFIX \
-      -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_INSTALL_LIBDIR=lib \
-      -DBUILD_SHARED_LIBS=ON \
-      -DDYLIB_COMPAT_VERSION:STRING="12.0.0" \
-      $SRC_DIR
+      -DCMAKE_BUILD_TYPE=Release
 
-ninja -j${CPU_COUNT}
-ninja install
+# Build
+cmake --build build --config Release
 
-# Add sdl.pc to support old pkg-config
-# See https://github.com/libsdl-org/sdl12-compat/issues/162
-cp $PREFIX/lib/pkgconfig/sdl12_compat.pc $PREFIX/lib/pkgconfig/sdl.pc
+# Install
+cmake --install build --config Release --prefix $PREFIX
