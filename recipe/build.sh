7dedc8d7758a56c6cb03d3364a21342e4d201dd4
Yann de Mont-Marin <ydemontm@mac.home>
2025-02-13 14:06:22 +0100

    Use additional options to make the setup correct for osx

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,25 @@
 #!/bin/sh
 
+if [[ ${HOST} =~ .*darwin.* ]]; then
+  # Adapt cflags for the sdk in use
+  export CFLAGS="${CFLAGS} -isysroot ${SDKROOT:-$CONDA_BUILD_SYSROOT}"
+
+  # Additional build option depending on target architecture
+  if [[ "${target_platform}" == "osx-arm64" ]]; then
+    export ADDITIONAL_OPTIONS="-DCMAKE_OSX_ARCHITECTURES=arm64 -DCMAKE_OSX_DEPLOYMENT_TARGET=11.0"
+  fi
+  if [[ "${target_platform}" == "osx-x86_64" ]]; then
+    export ADDITIONAL_OPTIONS="-DCMAKE_OSX_ARCHITECTURES=x86_64 -DCMAKE_OSX_DEPLOYMENT_TARGET=10.13"
+  fi
+fi
+
 # Configure using the CMakeFiles
 cmake -S . -B build ${CMAKE_ARGS} \
       -DCMAKE_PREFIX_PATH=$PREFIX \
       -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_INSTALL_LIBDIR=lib \
-      -DCMAKE_BUILD_TYPE=Release
+      -DCMAKE_BUILD_TYPE=Release \
+      $ADDITIONAL_OPTIONS
 
 # Build
 cmake --build build --config Release
