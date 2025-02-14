7d0bd8fcbdaec2e136ff204eafddd4dfbc8c2313
Yann de Mont-Marin <yann.montmarin@gmail.com>
2025-02-14 15:27:18 +0100

    Simplify build.sh thanks to CMAKE_ARGS

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,25 +1,20 @@
 #!/bin/sh
 
 if [[ ${HOST} =~ .*darwin.* ]]; then
-  # Adapt cflags for the sdk in use
-  export CFLAGS="${CFLAGS} -isysroot ${SDKROOT:-$CONDA_BUILD_SYSROOT}"
-
-  # Additional build option depending on target architecture
+  # Additional build option depending on target architecture for SDL see:
+  # https://github.com/libsdl-org/SDL/blob/c16b7bcb7acb35c5b91153e6cd6b0da847394a09/docs/README-cmake.md?plain=1#L46
   if [[ "${target_platform}" == "osx-arm64" ]]; then
-    export ADDITIONAL_OPTIONS="-DCMAKE_OSX_ARCHITECTURES=arm64 -DCMAKE_OSX_DEPLOYMENT_TARGET=11.0"
+    export SDL_ARCHITECTURE_FLAG="-DCMAKE_OSX_ARCHITECTURES=arm64"
   fi
-  if [[ "${target_platform}" == "osx-x86_64" ]]; then
-    export ADDITIONAL_OPTIONS="-DCMAKE_OSX_ARCHITECTURES=x86_64 -DCMAKE_OSX_DEPLOYMENT_TARGET=10.13"
+  if [[ "${target_platform}" == "osx-64" ]]; then
+    export SDL_ARCHITECTURE_FLAG="-DCMAKE_OSX_ARCHITECTURES=x86_64"
   fi
 fi
 
 # Configure using the CMakeFiles
 cmake -S . -B build ${CMAKE_ARGS} \
       -DCMAKE_PREFIX_PATH=$PREFIX \
-      -DCMAKE_INSTALL_PREFIX=$PREFIX \
-      -DCMAKE_INSTALL_LIBDIR=lib \
-      -DCMAKE_BUILD_TYPE=Release \
-      $ADDITIONAL_OPTIONS
+      $SDL_ARCHITECTURE_FLAG
 
 # Build
 cmake --build build --config Release
