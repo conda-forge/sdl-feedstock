8a642b429490e7988dd0ce03ebda12d8c2175a3f
Yann de Mont-Marin <yann.montmarin@gmail.com>
2025-02-14 15:43:55 +0100

    Update build.sh to it simplest

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,16 +1,5 @@
 #!/bin/sh
 
-if [[ ${HOST} =~ .*darwin.* ]]; then
-  # Additional build option depending on target architecture for SDL see:
-  # https://github.com/libsdl-org/SDL/blob/c16b7bcb7acb35c5b91153e6cd6b0da847394a09/docs/README-cmake.md?plain=1#L46
-  if [[ "${target_platform}" == "osx-arm64" ]]; then
-    export SDL_ARCHITECTURE_FLAG="-DCMAKE_OSX_ARCHITECTURES=arm64"
-  fi
-  if [[ "${target_platform}" == "osx-64" ]]; then
-    export SDL_ARCHITECTURE_FLAG="-DCMAKE_OSX_ARCHITECTURES=x86_64"
-  fi
-fi
-
 # Configure using the CMakeFiles
 cmake -S . -B build ${CMAKE_ARGS} \
       -DCMAKE_PREFIX_PATH=$PREFIX \
