7ce0079a85e248e4fb493f56ae0d36cdc5e6c29b
Yann de Mont-Marin <ydemontm@wifi-pro-83-203.paris.inria.fr>
2025-02-17 16:22:58 +0100

    Update sdl2 to use compat2 layer on sdl3

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,22 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
-# Get an updated config.sub and config.guess
-cp $BUILD_PREFIX/share/gnuconfig/config.* ./build-scripts
+# Configure using the CMakeFiles
+cmake -S . -B build ${CMAKE_ARGS} -DCMAKE_PREFIX_PATH=$PREFIX
 
-# we have to do this because most build scripts assume that all sdl modules
-# are installed to the same prefix.
-sed -i -- "s|@prefix@|${PREFIX}|g" sdl2.pc.in 
-sed -i -- "s|@prefix@|${PREFIX}|g" sdl2-config.in
+# Build
+cmake --build build --config Release
 
-export PULSEAUDIO_CFLAGS=${PREFIX}/include
-export PULSEAUDIO_LIBS=${PREFIX}/lib
-
-# Build SDL2
-./autogen.sh
-if [ -z ${OSX_ARCH+x} ]; then
-  ./configure --prefix=${PREFIX} --enable-video-wayland=no;
-else
-  ./configure --prefix=${PREFIX} --without-x LDFLAGS="-framework ForceFeedback" --enable-video-wayland=no;
-fi
-
-make install
+# Install
+cmake --install build --config Release --prefix $PREFIX
