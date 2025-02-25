0328ec7d98afe761efcf37c2bc66bca39bca82de
Silvio Traversaro <silvio@traversaro.it>
2025-02-25 09:29:12 +0100

    Copy sdl2_compat.pc to sdl2.pc for compatibility with old pkg-config used in conda-forge

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -14,3 +14,8 @@ if errorlevel 1 exit 1
 :: Install!
 cmake --install build --config Release --prefix "%LIBRARY_PREFIX%"
 if errorlevel 1 exit 1
+
+:: Add sdl.pc to support old pkg-config
+:: See https://github.com/libsdl-org/sdl12-compat/issues/162 and
+:: https://github.com/libsdl-org/sdl2-compat/issues/381
+copy %LIBRARY_PREFIX%\lib\pkgconfig\sdl2_compat.pc %LIBRARY_PREFIX%\lib\pkgconfig\sdl2.pc
